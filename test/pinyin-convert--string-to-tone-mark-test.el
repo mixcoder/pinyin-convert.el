e43780a95d0395bb16168e0c76ae81dc1acb0213
tprost <tprost@users.noreply.github.com>
2016-02-12 20:19:55 -0500

    refactor `string-to-tone-mark` test

--- test/pinyin-convert--string-to-tone-mark-test.el
+++ test/pinyin-convert--string-to-tone-mark-test.el
@@ -1,22 +1,19 @@
-(ert-deftest pinyin-convert--string-to-tone-mark/basic-pinyin ()
-  "Should convert giant string of basic pinyin with no spaces."
-  (should (equal
-           (mapconcat 'concat basic-tone-mark-pinyin "")
-           (pinyin-convert--string-to-tone-mark
-            (mapconcat 'concat basic-tone-number-pinyin "")))))
-
-(ert-deftest pinyin-convert--string-to-tone-mark/erhua-pinyin ()
-  "Should convert giant string of erhua pinyin with no spaces."
-  (should (equal
-           (mapconcat 'concat erhua-tone-mark-pinyin "")
-           (pinyin-convert--string-to-tone-mark
-            (mapconcat 'concat erhua-tone-number-pinyin "")))))
-
 (ert-deftest pinyin-convert--string-to-tone-mark/non-pinyin ()
   "Should not alter text that isn't tone number pinyin."
   (should (equal
            (pinyin-convert--string-to-tone-mark "hello")
-           "hello"))
+           "hello")))
+
+(ert-deftest pinyin-convert--string-to-tone-mark/basic-pinyin ()
+  "Should convert giant string of basic pinyin with spaces."
   (should (equal
-           (pinyin-convert--string-to-tone-mark "ma1 cat ma1")
-           "mā cat mā")))
+           (mapconcat 'concat basic-tone-mark-pinyin " ")
+           (pinyin-convert--string-to-tone-mark
+            (mapconcat 'concat basic-tone-number-pinyin " ")))))
+
+(ert-deftest pinyin-convert--string-to-tone-mark/erhua-pinyin ()
+  "Should convert giant string of erhua pinyin with spaces."
+  (should (equal
+           (mapconcat 'concat erhua-tone-mark-pinyin " ")
+           (pinyin-convert--string-to-tone-mark
+            (mapconcat 'concat erhua-tone-number-pinyin " ")))))
