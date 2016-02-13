67d669bfc9fd1711022be980ee0cba8c74ffc18e
tprost <tprost@users.noreply.github.com>
2016-02-13 09:45:55 -0500

    update docstrings in `test-helper.el`

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,13 +1,13 @@
 (require 'pinyin-convert)
 
 (defun read-lines (file-path)
-  "Return a list of lines of a file at file-path."
+  "Return a list of lines of a file at `file-path'."
   (with-temp-buffer
     (insert-file-contents file-path)
     (split-string (buffer-string) "\n" t)))
 
 (defun read-pinyin (file)
-  "Return a list of pairs of pinyin syllables from file in `/test/fixtures/pinyin`."
+  "Return a list of pairs of pinyin syllables from fixtures."
   (mapcar
    'split-string
    (read-lines
@@ -20,21 +20,33 @@
           (read-pinyin "tone-2.txt")
           (read-pinyin "tone-3.txt")
           (read-pinyin "tone-4.txt"))
-  "A list of pairs of legal pinyin.")
+  "An association list of basic pinyin.  An association list
+mapping each tone mark pinyin syllable using tone numbers 1, 2, 3
+or 4, to a list of possible tone number pinyin representations.")
 
 (defconst basic-tone-mark-pinyin
-  (mapcar 'car basic-pinyin))
+  (mapcar 'car basic-pinyin)
+  "A list of all basic tone mark pinyin.
+Contains all tone mark pinyin for tone numbers 1, 2, 3 and 4.")
 
 (defconst basic-tone-number-pinyin
-  (apply 'append (mapcar (lambda (syllables) (cdr syllables)) basic-pinyin)))
+  (apply 'append (mapcar (lambda (syllables) (cdr syllables)) basic-pinyin))
+  "A list of all basic tone number pinyin.
+Contains all tone number pinyin for tone numbers 1, 2, 3 and 4.")
 
-(defconst neutral-pinyin (read-pinyin "tone-5.txt"))
+(defconst neutral-pinyin
+  (read-pinyin "tone-5.txt")
+  "An association list of neutral pinyin.  An association list
+mapping each neutral tone tone mark pinyin to a list of possible
+tone number pinyin representations.")
 
 (defconst neutral-tone-mark-pinyin
-  (mapcar 'car neutral-pinyin))
+  (mapcar 'car neutral-pinyin)
+  "A list of all tone neutral tone mark pinyin.")
 
 (defconst neutral-tone-number-pinyin
-  (apply 'append (mapcar (lambda (syllables) (cdr syllables)) neutral-pinyin)))
+  (apply 'append (mapcar (lambda (syllables) (cdr syllables)) neutral-pinyin))
+  "A list of all tone neutral tone number pinyin.")
 
 (defconst erhua-pinyin
   (mapcar
@@ -44,10 +56,16 @@
                        (concat (substring syllable 0 -1)
                                "r"
                                (substring syllable -1 nil)))
-                     (cdr pinyin)))) basic-pinyin))
+                     (cdr pinyin)))) basic-pinyin)
+  "An association list for all erhua pinyin.  An association list
+mapping each erhua tone mark pinyin to a list of possible tone
+number pinyin representations. It is constructed essentially by
+appending an `r' to existing pinyin.")
 
 (defconst erhua-tone-mark-pinyin
-  (mapcar 'car erhua-pinyin))
+  (mapcar 'car erhua-pinyin)
+  "A list of all erhua tone mark pinyin.")
 
 (defconst erhua-tone-number-pinyin
-  (apply 'append (mapcar (lambda (syllables) (cdr syllables)) erhua-pinyin)))
+  (apply 'append (mapcar (lambda (syllables) (cdr syllables)) erhua-pinyin))
+  "A list of all erhua tone number pinyin.")
